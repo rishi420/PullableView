9dfcc9820192feb3ffcec686477053255292de7f
Warif Rishi <rishi420@gmail.com>
2013-09-08 15:07:31 +0600

    Open center close center auto calculation

--- PullableView/StyledPullableView.m
+++ PullableView/StyledPullableView.m
@@ -11,7 +11,7 @@
     if ((self = [super initWithFrame:frame])) {
         
         UIImageView *imgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background.png"]];
-        imgView.frame = CGRectMake(0, 0, 320, 460);
+        imgView.frame = CGRectMake(0, 0, frame.size.width, frame.size.height);
         [self addSubview:imgView];
         [imgView release];
     }
--- PullableView/ViewController.m
+++ PullableView/ViewController.m
@@ -5,6 +5,9 @@
  @author Fabio Rodella fabio@crocodella.com.br
  */
 
+#define PUlL_UP_VIEW_HEIGHT     400
+#define PULL_DOWN_VIEW_HEIGHT   300
+
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -16,46 +19,46 @@
         xOffset = 224;
     }
     
-    pullRightView = [[PullableView alloc] initWithFrame:CGRectMake(0, 200, 200, 300)];
-    pullRightView.backgroundColor = [UIColor lightGrayColor];
-    pullRightView.openedCenter = CGPointMake(100, 200);
-    pullRightView.closedCenter = CGPointMake(-70, 200);
-    pullRightView.center = pullRightView.closedCenter;
-    pullRightView.animate = NO;
+    //    pullRightView = [[PullableView alloc] initWithFrame:CGRectMake(0, 200, 200, 300)];
+    //    pullRightView.backgroundColor = [UIColor lightGrayColor];
+    //    pullRightView.openedCenter = CGPointMake(100, 200);
+    //    pullRightView.closedCenter = CGPointMake(-70, 200);
+    //    pullRightView.center = pullRightView.closedCenter;
+    //    pullRightView.animate = NO;
+    //
+    //    pullRightView.handleView.backgroundColor = [UIColor darkGrayColor];
+    //    pullRightView.handleView.frame = CGRectMake(170, 0, 30, 300);
+    //
+    //    [self.view addSubview:pullRightView];
+    //    [pullRightView release];
+    //
+    //    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 30)];
+    //    label.backgroundColor = [UIColor darkGrayColor];
+    //    label.textColor = [UIColor whiteColor];
+    //    label.text = @"Pull me to the right!";
+    //    label.transform = CGAffineTransformMakeRotation(-M_PI_2);
+    //    label.center = CGPointMake(185, 150);
+    //
+    //    [pullRightView addSubview:label];
+    //    [label release];
+    //
+    //    label = [[UILabel alloc] initWithFrame:CGRectMake(4, 120, 200, 30)];
+    //    label.backgroundColor = [UIColor clearColor];
+    //    label.textColor = [UIColor whiteColor];
+    //    label.text = @"I'm not animated";
+    //
+    //    [pullRightView addSubview:label];
+    //    [label release];
     
-    pullRightView.handleView.backgroundColor = [UIColor darkGrayColor];
-    pullRightView.handleView.frame = CGRectMake(170, 0, 30, 300);
-    
-    [self.view addSubview:pullRightView];
-    [pullRightView release];
-    
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 30)];
-    label.backgroundColor = [UIColor darkGrayColor];
-    label.textColor = [UIColor whiteColor];
-    label.text = @"Pull me to the right!";
-    label.transform = CGAffineTransformMakeRotation(-M_PI_2);
-    label.center = CGPointMake(185, 150);
-    
-    [pullRightView addSubview:label];
-    [label release];
-    
-    label = [[UILabel alloc] initWithFrame:CGRectMake(4, 120, 200, 30)];
-    label.backgroundColor = [UIColor clearColor];
-    label.textColor = [UIColor whiteColor];
-    label.text = @"I'm not animated";
-    
-    [pullRightView addSubview:label];
-    [label release];
-    
-    pullUpView = [[StyledPullableView alloc] initWithFrame:CGRectMake(xOffset, 0, 320, 460)];
-    pullUpView.openedCenter = CGPointMake(160 + xOffset,self.view.frame.size.height);
-    pullUpView.closedCenter = CGPointMake(160 + xOffset, self.view.frame.size.height + 200);
+    pullUpView = [[StyledPullableView alloc] initWithFrame:CGRectMake(xOffset, 0, 320, PUlL_UP_VIEW_HEIGHT)];
+    pullUpView.openedCenter = CGPointMake(160 + xOffset, (self.view.frame.size.height - PUlL_UP_VIEW_HEIGHT / 2));
+    pullUpView.closedCenter = CGPointMake(160 + xOffset, self.view.frame.size.height + PUlL_UP_VIEW_HEIGHT / 2 - 30);
     pullUpView.center = pullUpView.closedCenter;
     pullUpView.handleView.frame = CGRectMake(0, 0, 320, 40);
     pullUpView.delegate = self;
     
     [self.view addSubview:pullUpView];
-    [pullUpView release];    
+    [pullUpView release];
     
     pullUpLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 4, 320, 20)];
     pullUpLabel.textAlignment = UITextAlignmentCenter;
@@ -66,7 +69,7 @@
     [pullUpView addSubview:pullUpLabel];
     [pullUpLabel release];
     
-    label = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, 320, 64)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 80, 320, 64)];
     label.textAlignment = UITextAlignmentCenter;
     label.backgroundColor = [UIColor clearColor];
     label.textColor = [UIColor whiteColor];
@@ -77,9 +80,9 @@
     [pullUpView addSubview:label];
     [label release];
     
-    pullDownView = [[StyledPullableView alloc] initWithFrame:CGRectMake(xOffset, 0, 320, 460)];
-    pullDownView.openedCenter = CGPointMake(160 + xOffset,230);
-    pullDownView.closedCenter = CGPointMake(160 + xOffset, -200);
+    pullDownView = [[StyledPullableView alloc] initWithFrame:CGRectMake(xOffset, 0, 320, PULL_DOWN_VIEW_HEIGHT)];
+    pullDownView.openedCenter = CGPointMake(160 + xOffset, PULL_DOWN_VIEW_HEIGHT / 2);
+    pullDownView.closedCenter = CGPointMake(160 + xOffset, - PULL_DOWN_VIEW_HEIGHT / 2 + 30);
     pullDownView.center = pullDownView.closedCenter;
     
     [self.view addSubview:pullDownView];
@@ -96,6 +99,7 @@
     [pullDownView addSubview:label];
     [label release];
 }
+
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
